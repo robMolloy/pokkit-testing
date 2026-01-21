fcaeba3585a342996ae7eff581fcb3b15445e70f
robmolloy <robmolloy@hotmail.co.uk>
2026-01-21 23:20:37 +0000

    working setup with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig, loadEnv } from "vite";
 import type { ConfigEnv } from "vite";
+import { defineConfig } from "vitest/config";
+import { loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import Pages from "vite-plugin-pages";
 
@@ -10,6 +11,10 @@ export default (p: { mode: ConfigEnv }) => {
 
   return defineConfig({
     base: env.VITE_APP_BASE_URL,
+    test: {
+      environment: "node", // important for PocketBase
+      globals: true, // allows describe/it/expect without imports
+    },
     plugins: [
       react(),
       tailwindcss(),
