3dfa4cea4d2c2f7e9c5c9b70d75b386484c1e510
robmolloy <robmolloy@hotmail.co.uk>
2026-01-25 23:36:12 +0000

    create test1 for globalUserPermissions => allow create: user with valid email and password

---
+++ src/tests/pocketbase.global-permissions.test.ts
@@ -0,0 +1,39 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { clearDatabase, createUserEmailPasswordData } from "./helpers/pocketbaseTestHelpers";
+import { testPb } from "../config/pocketbaseConfig";
+
+const usersCollectionName = "users";
+const globalUserPermissionsCollectionName = "globalUserPermissions";
+
+describe("PocketBase users collection global permissions", () => {
+  beforeEach(async () => {
+    await clearDatabase();
+  });
+
+  it.only("allow create: user with valid email and password", async () => {
+    const { email, password } = createUserEmailPasswordData();
+    const createdUser = await testPb.collection(usersCollectionName).create({
+      email,
+      password,
+      passwordConfirm: password,
+    });
+
+    await testPb.collection(usersCollectionName).authWithPassword(email, password);
+
+    expect(createdUser.id).not.toBeNull();
+
+    const createdGlobalUserPermissionsRecord = await testPb
+      .collection(globalUserPermissionsCollectionName)
+      .getOne(createdUser.id);
+
+    expect(createdGlobalUserPermissionsRecord.id).toBe(createdUser.id);
+    expect(createdGlobalUserPermissionsRecord.role).toBe("admin");
+    expect(createdGlobalUserPermissionsRecord.status).toBe("approved");
+
+    testPb.authStore.clear();
+
+    await expect(
+      testPb.collection(globalUserPermissionsCollectionName).getOne(createdUser.id),
+    ).rejects.toThrow();
+  });
+});
