4279f212e7c2a9b3ada74cde173f1f192daa0760
robmolloy <robmolloy@hotmail.co.uk>
2026-01-30 23:18:24 +0000

    remove console.log

--- src/config/pocketbaseConfig.ts
+++ src/config/pocketbaseConfig.ts
@@ -3,10 +3,6 @@ export { PocketBase };
 export type { RecordModel, RecordSubscription } from "pocketbase";
 import "dotenv/config";
 
-console.log(`src/config/pocketbaseConfig.ts:${/*LL*/ 5}`, {
-  TEST_DB_URL: import.meta.env,
-});
-
 export const userPb = new PocketBase(process.env.TEST_DB_URL);
 
 // no difference between userPb and superuserPb - just enforces naming convention consistency
