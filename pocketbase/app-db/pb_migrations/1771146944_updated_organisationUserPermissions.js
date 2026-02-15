defe22a708ebd476390015ed888a6dfbe836408f
robmolloy <robmolloy@hotmail.co.uk>
2026-02-15 21:59:06 +0000

    add organisationUserPermissions collection update rules tests for user roles

---
+++ pocketbase/app-db/pb_migrations/1771146944_updated_organisationUserPermissions.js
@@ -0,0 +1,20 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_230240349")
+
+  // update collection data
+  unmarshal({
+    "updateRule": ""
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_230240349")
+
+  // update collection data
+  unmarshal({
+    "updateRule": "@request.auth.id != \"\" && \n@collection.organisationUserPermissions.userId ?= @request.auth.id && \n@collection.organisationUserPermissions.organisationId ?= organisationId && \n@collection.organisationUserPermissions.role = \"admin\""
+  }, collection)
+
+  return app.save(collection)
+})
